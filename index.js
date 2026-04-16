dc7ea21a9e4c93097e82df0d973992794de45631
ThisisAlam <103932559+ThisisAlam@users.noreply.github.com>
2026-04-16 11:56:19 +0500

    updated the javascript, html and css

--- index.js
+++ index.js
@@ -6,13 +6,23 @@
 //     <button id="btn">BoredBot</button>`
 
 // document.body.appendChild(div)
-
+const text = document.getElementById('text')
 const btn = document.getElementById('btn')
 btn.addEventListener('click', () => {
     fetch('https://apis.scrimba.com/bored/api/activity')
         .then(response => response.json())
         .then(data => {
             console.log(data)
-            document.getElementById('text').innerText = data.activity    
+            text.innerText = data.activity
         })  
+    fetch('https://x-colors.yurace.pro/api/random/{all}?type=dark')
+        .then(response => response.json())
+        .then(color => {
+            console.log(color)
+            btn.style.background = `${color.hex}`
+            text.style.color = `${color.hex}`
+            text.style.borderTopColor = `${color.hex}`
+            text.style.borderBottomColor = `${color.hex}`
+        })
+
 })
